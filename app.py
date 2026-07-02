d5a26124638e34495fa158bf35bcda29d3ab8b02
euroboygt-rgb <euroboygt@gmail.com>
2026-07-02 14:33:44 -0600

    Add Bootstrap layout and navigation

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-
+@app.route("/labs")
+def labs():
+    return render_template("labs.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
