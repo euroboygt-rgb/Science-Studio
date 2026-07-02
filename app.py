d0192d4af73dcde4380a67cf7443180a7f820844
euroboygt-rgb <euroboygt@gmail.com>
2026-07-02 16:05:11 -0600

    Add first nine weeks curriculum page

--- app.py
+++ app.py
@@ -10,5 +10,8 @@ def home():
 def labs():
     return render_template("labs.html")
 
+@app.route("/first-nine-weeks")
+def first_nine_weeks():
+    return render_template("first_nine_weeks.html")
 if __name__ == "__main__":
     app.run(debug=True)
