6dc37e916e47feb24d03c338ba98321f0eaace83
euroboygt-rgb <euroboygt@gmail.com>
2026-07-05 22:25:10 -0600

    Add Day 39 ball bounce data analysis and graphing lesson

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ from curriculum.day35 import day35
 from curriculum.day36 import day36
 from curriculum.day37 import day37
 from curriculum.day38 import day38
+from curriculum.day39 import day39
 
 app = Flask(__name__)
 
@@ -81,6 +82,7 @@ lesson_details = {
     36: day36,
     37: day37,
     38: day38,
+    39: day39,
 }
 
 
